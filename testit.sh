5d8cf686fafcd0ea7464dbbcb6a03b4fbf3a0b26
Pathway <peter.wilks@pathway-technologies.com>
2021-02-11 09:21:34 +0000

    Update testit.sh

--- testit.sh
+++ testit.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo Test script for the linux-cpp docker container
+
+# Look for the Boost folders
+ls -l usr/local/include/
+ls -l usr/local/include/boost
+ls -l usr/local/lib/
+ls -l usr/local/lib/boost
